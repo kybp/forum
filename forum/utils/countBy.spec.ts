bd4239b27d4a0a3e283b976b129ddd8f9b1a39f1
Kyle Brown <kyle@panicky.net>
2024-02-01 09:02:51 -0600

    Get reply editing working

---
+++ forum/utils/countBy.spec.ts
@@ -0,0 +1,19 @@
+import { it, expect } from 'vitest'
+import { countBy } from './countBy'
+
+  it('returns a count of elements by key', () => {
+    const elements = [
+      { key: 'one' },
+      { key: 'three' },
+      { key: 'two' },
+      { key: 'three' },
+      { key: 'two' },
+      { key: 'three' },
+    ]
+
+    expect(countBy(elements, (x) => x.key)).toEqual({
+      one: 1,
+      two: 2,
+      three: 3,
+    })
+  })
