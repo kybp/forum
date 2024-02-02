7b86fd06d6d4e222fe887a3e2e8ac0d92b0f6091
Kyle Brown <kyle@panicky.net>
2024-02-01 19:52:01 -0600

    Add account registration

--- forum/stores/users.ts
+++ forum/stores/users.ts
@@ -1,8 +1,31 @@
 import { defineStore } from 'pinia'
-import type { User } from '@/api'
+import type { Account, User } from '@/api'
+
+export type CreateUserParams = {
+  username: string
+  email: string
+  password: string
+}
 
 export const useUsersStore = defineStore('users', () => {
+  const authStore = useAuthStore()
+
   const users = ref<Record<number, User>>({})
+
+  const createUser = async (
+    params: CreateUserParams,
+  ): Promise<AsyncResponse<User>> => {
+    const response = await useFetch<Account>(apiUrl('users/accounts/'), {
+      method: 'POST',
+      body: params,
+    })
+
+    const account = response.data.value
+    authStore.setAccount(account)
+    if (account) users.value[account.id] = account
+
+    return response
+  }
 
   const getUser = async (id: number): Promise<AsyncResponse<User>> => {
     const response = await useFetch<User>(apiUrl(`users/users/${id}/`))
@@ -19,7 +42,10 @@ export const useUsersStore = defineStore('users', () => {
   }
 
   return {
+    createUser,
     getUser,
     findUser,
   }
 })
+
+export type UsersStore = ReturnType<typeof useUsersStore>
